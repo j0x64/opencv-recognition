f5958d12cc6950045b10576a4f08182b77f485b4
Jordan <alhamdifits2@gmail.com>
2024-05-18 06:45:04 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+## 📷 OpenCV Recognition
+**OpenCV** is a deep learning machine library in Python intended in camera and such. This project uses **OpenCV** as a way to track face and eyes in realtime.
+## 📃 Documentation
+If you wanted to learn more about OpenCV deep learning machine in Python, you can view their docs in the official website. [Click here to learn more](https://docs.opencv.org/4.x/index.html)
+## 📜 Dependencies
+- Python 3.12
+- OpenCV-Python
+## 👤 Contact
+- [JordanIsADev](https://github.com/jordanisadev) (Github)
---
+++ main.py
@@ -0,0 +1,40 @@
+# OpenCV-Recognition by Jordan (C) JordanIsADev
+# Any suggestions or critics are welcomed
+
+# Importing necessary libraries
+import cv2
+
+# Creating functions to handle eyes and face module
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+
+# Define the video capture
+cap = cv2.VideoCapture(0)
+
+# Main function (loop the webcamera)
+while True:
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        cv2.putText(frame, 'Face', (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+        
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (255, 0, 0), 2)
+            # Add text indicating eyes detected
+            cv2.putText(frame, 'Eyes', (x + ex, y + ey - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+
+    cv2.imshow('Face and Eye Detection', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the video capture object and close all windows
+cap.release()
+cv2.destroyAllWindows()
